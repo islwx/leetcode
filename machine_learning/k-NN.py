f996767a335846cff7a99090838301c8012f6ed5
weixiong <weixiong.lin@moffett.ai>
2022-04-18 01:46:13 +0800

    add k-NN

--- machine_learning/k-NN.py
+++ machine_learning/k-NN.py
@@ -2,87 +2,80 @@ import numpy as np
 import pandas as pd
 from easydict import EasyDict
 from collections import Counter
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from pandas import DataFrame
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+import time
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-class Case:
-    def __init__(self, feat, label, d=float('-inf')):
-        self.feat = feat
-        self.label = label
-        self.d=d
 
-class KDTreeNode:
-    def __init__(self, cases, medians, parent=None):
-        self._dim = None
-        self.parent = parent
-        self.cases = cases
-        self.median = medians[self.dim]
-        self._build_sub_node(medians)
-    
+class LpDistance(object):
+    def __init__(self, p):
+        self.func=None
+        self.p = p
     @property
-    def dim(self):
-        if self._dim is None:
-            if self.parent is None:
-                self._dim = 0
-            else:
-                self._dim = self.parent.dim + 1
-        return self._dim
-
-    @staticmethod
-    def split_case(iswhere, cases):
-        new_cases = EasyDict()
-        for key in cases:
-            new_cases[key] = cases[key][iswhere]
-        
-            
-        return new_cases
-            
-
-    def _build_sub_node(self, medians):
-        if self.dim==medians.size-1:
-            self.left =None
-            self.right=None
+    def p(self):
+        return self._p
+    @p.setter
+    def p(self, p):
+        self._p = p
+        inf = float('inf')
+        if self.p == inf:
+            self.func = lambda a_feat, b_feats: (a_feat-b_feats).abs().max(axis=1)
+        elif self.p == 1:
+            self.func = lambda a_feat, b_feats: (a_feat-b_feats).abs().sum(axis=1)
         else:
-            is_left = self.cases.feat[:, self.dim]> self.median    
-            left_cases = self.split_case(is_left, self.cases)
-            right_cases = self.split_case(~is_left, self.cases)
-
-            if len(left_cases.feat)>0:
-                self.left = self.__class__(left_cases, medians, self)
-            else:
-                self.left = None
-            if len(right_cases.feat)>0:
-                self.right = self.__class__(right_cases, medians, self)
-            else:
-                self.right = None
-
+            self.func = lambda a_feat, b_feats: ((a_feat-b_feats)**self.p).sum(axis=1)**(1/self.p)
+    def __call__(self, a_feats, b_feats):
+        assert a_feats.ndim == b_feats.ndim, (a_feats.ndim, b_feats.ndim)
+        distances = []
+        for i in range(a_feats.shape[0]):
+            a_feat = a_feats[i]
+            distances.append(self.func(a_feat, b_feats))
+        return distances
     
-    def search_nn_node(self, feat):
-        assert feat.size == self.cases.feat[0].size,(feat.size, self.cases.feat[0].size)
-        if feat[self.dim]>self.median:
-            if self.left is None:
-                return self
-            else:
-                return self.left.search_nn_node(feat)
-        else:
-            if self.right is None:
-                return self
-            else:
-                return self.right.search_nn_node(feat)
+class KNN:
+    def __init__(self, k: int=10):
+        self.k: int = k
+        self.data: Optional[np.ndarray] = None
+        self.target: Optional[np.ndarray] = None
+        self.calc_distance = LpDistance(p=2)
+
+    def fit(self, data_: np.ndarray, target_: np.ndarray, train_val=True) -> None:
+        if train_val:
+            result = []
+            trian_data, val_data, train_target, val_target = train_test_split(data_, target_, test_size=0.33, random_state=10)
+            self.data=trian_data
+            self.target=train_target
+            k_max = len(train_target)
+            for cur_k in range(1, k_max+1):
+                self.k = cur_k
+                predictions = self.predict(val_data)
+                acc = accuracy_score(predictions, val_target)
+                print([cur_k, acc])
+                result.append([cur_k, acc])
+            self.k=sorted(result,reverse=True, key=lambda x:x[1])[0]
+            print(f"k:{self.k}")
+        self.data = data_
+        self.target = target_
     
-        
+    def _predict(self, dis):
+        distances = dis.argsort()[:self.k]
+        counter = Counter([self.target.iloc[index] for index in distances])
+        return counter.most_common(1)[0][0]
 
-def Lp_distance(a_feat, b_feats, p=0):
-    inf = float('inf')
-    if isinstance(b_feats, list):
-        b_feats = np.asarray(b_feats)
-    assert a_feat.shape == b_feats[0].shape
-    if p == inf:
-        return np.abs((a_feat-b_feats)).max(axis=1)
-    elif p == 1:
-        return np.abs((a_feat-b_feats)).sum(axis=1)
-    else:
-        return ((a_feat-b_feats)**p).sum(axis=1)**(1/p)
+    def predict(self, data_: np.ndarray) -> int:
+        all_distances = self.calc_distance(data_, self.data)
+        return [self._predict(distances) for distances in all_distances]
+    
+    
 
-class KDTree:
+class KDTree_bak:
     def __init__(self, cases, k=10):
         self.cases = cases
         self.medians = np.median(self.cases.feat, axis=0)
@@ -101,8 +94,6 @@ class KDTree:
             cur_nn_cases 
             for nn_cases in sortd(nn_cases, key=lambda cases:case.d):
                 pass
-
-
             
             
 
@@ -137,24 +128,182 @@ class KDTree:
         nn_cases = self._infer_nn_cases(feat, root_nn_node, None)
         c=Counter(nn_cases.label)
         return c.most_common(1)[0][0]
-        
+class KDTreeNode:
+    def __init__(self, cases, medians, parent=None):
+        self._dim = None
+        self.parent = parent
+        self.cases = cases
+        self.median = medians[self.dim]
+        self._build_sub_node(medians)
     
+    @property
+    def dim(self):
+        if self._dim is None:
+            self._dim = 0 if self.parent is None else self.parent.dim + 1
+        return self._dim
 
-def example():
-    num_case = 50
-    num_axis = 10
-    feats = np.random.rand(num_case,num_axis)
-    feat = np.random.rand(num_axis)
-    labels = (np.random.rand(num_case)*3).round().astype(int)
-    cases = EasyDict({
-        'feat':feats,
-        'label':labels,
-        # 'd':np.full_like(feats, float('inf'))
-    })
-    # cases = pd.DataFrame(data,columns=['feat','label','d'])
-    kdt = KDTree(cases)
-    label = kdt.infer(feat)
-    print(label)
+    @staticmethod
+    def split_case(iswhere, cases):
+        new_cases = EasyDict()
+        for key in cases:
+            new_cases[key] = cases[key][iswhere]
+        
+            
+        return new_cases
+            
+
+    def _build_sub_node(self, medians):
+        if self.dim==medians.size-1 or  \
+            self.cases.labels[1:]==self.cases.labels[-1:]:
+            self.left =None
+            self.right=None
+        else:
+            is_left = self.cases.feat[:, self.dim]> self.median    
+            left_cases = self.split_case(is_left, self.cases)
+            right_cases = self.split_case(~is_left, self.cases)
+
+            if len(left_cases.feat)>0:
+                self.left = self.__class__(left_cases, medians, self)
+            else:
+                self.left = None
+            if len(right_cases.feat)>0:
+                self.right = self.__class__(right_cases, medians, self)
+            else:
+                self.right = None
+
+    
+    def search_nn_node(self, feat):
+        assert feat.size == self.cases.feat[0].size,(feat.size, self.cases.feat[0].size)
+        if feat[self.dim]>self.median:
+            return self if self.left is None else self.left.search_nn_node(feat)
+        else:
+            return self if self.right is None else self.right.search_nn_node(feat)
+    
+class KDTreeNode:
+    def __init__(self, feats=np.empty(0), labels=np.empty(0), parent=None):
+        self.feats:np.ndarray = feats
+        self.labels:np.ndarray = labels
+        self.median = -1
+        self.dim = -1
+        self.parent = parent
+        self.sub_nodes = []
+        self.build_sub_node()
+    
+    def build_sub_node(self):
+        logger.debug(f"size: {len(self)}, same_label:{(self.labels == self.labels[0]).all()}")
+        if len(self)>0 and (self.labels != self.labels[0]).any():
+            logger.debug("build sub nodes...")
+            std = self.feats.std(axis=0)
+            dim = std.argmax()
+            median =  np.median(self.feats[:, dim])
+            logger.debug(f"sub nodes: std:{std}, dim:{dim}, median:{median}")
+            is_left = self.feats[:, dim]>median
+            left_feats = self.feats[is_left, :]
+            left_labels = self.labels[is_left]
+            self.sub_nodes.append(self.__class__(left_feats, left_labels, self))
+
+            right_feats = self.feats[~is_left, :]
+            right_labels = self.labels[~is_left]
+            self.sub_nodes.append(self.__class__(right_feats, right_labels, self))
+
+            self.dim = dim
+            self.median=median
+        else:
+            logger.debug(f"dont build:{(self.labels == self.labels[0])}")
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def deep(self):
+        if not self.sub_nodes:
+            return 1, 1
+        deep0, counts0 = self.sub_nodes[0].deep()
+        deep1, counts1 = self.sub_nodes[1].deep()
+        return max(deep0, deep1)+1, counts0+counts1+1
+
+    def search_nn_node(self, feat):
+        if not sub_nodes:
+            return self
+        if feat[dim]>self.median:
+            return self.sub_nodes[0].search_nn_node(feat)
+        else:
+            return self.sub_nodes[1].search_nn_node(feat)
+            
+
+class KDTree:
+    def __init__(self, k: int=10):
+        self.k: int = k
+        self.root = None
+        self.calc_distance = LpDistance(p=2)
+
+    def build(self, feats, labels):
+        self.root = KDTreeNode(feats, labels)
+        logger.critical(self.root.deep())
+
+    def fit(self, feats: np.ndarray, labels: np.ndarray, train_val=False) -> None:
+        if train_val:
+            result = []
+            trian_data, val_data, train_target, val_target = train_test_split(data_, target_, test_size=0.33, random_state=10)
+            self.data=trian_data
+            self.target=train_target
+            k_max = len(train_target)
+            for cur_k in range(1, k_max+1):
+                self.k = cur_k
+                predictions = self.predict(val_data)
+                acc = accuracy_score(predictions, val_target)
+                print([cur_k, acc])
+                result.append([cur_k, acc])
+            self.k=sorted(result,reverse=True, key=lambda x:x[1])[0][0]
+            print(f"k:{self.k}")
+        self.build(feats, labels)
+        
+    def search(self, feats):
+        for feat in feats:
+            nn_node = self.root.search_nn_node(feat)
+
+
+    def _predict(self, dis):
+        distances = dis.argsort()[:self.k]
+        counter = Counter([self.target.iloc[index] for index in distances])
+        return counter.most_common(1)[0][0]
+
+    def predict(self, data_: np.ndarray) -> int:
+        all_distances = self.calc_distance(data_, self.data)
+        return [self._predict(distances) for distances in all_distances]        
+            
+
+        
+
+def example_knn():
+    wine = load_wine(return_X_y=True, as_frame=True)
+    data: DataFrame = wine[0]
+    target: DataFrame = wine[1]
+    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=1000)
+    scalar = MinMaxScaler()
+    x_train = scalar.fit_transform(x_train,)
+    x_test = scalar.fit_transform(x_test)
+    model = KDTree(k=10)
+    model.fit(x_train, y_train,train_val=True)
+    # model.fit(x_train, y_train,train_val=False)
+    predictions = model.predict(x_test)
+    print(accuracy_score(predictions, y_test))
+
+def example_kd_tree():
+    wine = load_wine(return_X_y=True, as_frame=True)
+    data: DataFrame = wine[0]
+    target: DataFrame = wine[1]
+    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=1000)
+    scalar = MinMaxScaler()
+    x_train = scalar.fit_transform(x_train)
+    x_test = scalar.fit_transform(x_test)
+    y_train = y_train.to_numpy()
+    y_test = y_test.to_numpy()
+    model = KDTree(k=10)
+    model.fit(x_train, y_train, train_val=False)
+    # model.fit(x_train, y_train,train_val=False)
+    # predictions = model.predict(x_test)
+    # print(accuracy_score(predictions, y_test))
+
 
 if __name__ == '__main__':
-    example()
+    example_kd_tree()
